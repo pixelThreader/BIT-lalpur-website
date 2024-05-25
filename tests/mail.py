6be0296e82a8ba726e2ac37a8b1b29d3d79aaad7
piyush0431 <rana.workslance@gmail.com>
2024-05-25 23:32:52 +0530

    update!!!

--- tests/mail.py
+++ tests/mail.py
@@ -1,31 +1,9 @@
-import subprocess
-import sys
-import pkg_resources
+from django.core.mail import send_mail
 
-def install(package):
-    try:
-        pkg_resources.get_distribution(package)
-        print(f"{package} is already installed")
-    except pkg_resources.DistributionNotFound:
-        print(f"Installing {package}...")
-        process = subprocess.Popen(
-            [sys.executable, "-m", "pip", "list"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-        )
-        while True:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
-                break
-            if output:
-                print(output.strip())
-        rc = process.poll()
-        if rc == 0:
-            print(f"Successfully installed {package}")
-        else:
-            error_output = process.stderr.read()
-            print(f"Failed to install {package}. Error: {error_output}")
-
-# Example usage
-install("python-dotenv")
+send_mail(
+    "Subject here",
+    "This is test form Django",
+    "piyush@piyush.com",
+    ["piyush31032005@gmail.com"],
+    fail_silently=False,
+)
