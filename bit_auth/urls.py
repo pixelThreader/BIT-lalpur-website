6aac3dbdfb8c1a96e98375b4fef8e8a902751611
piyush0431 <rana.workslance@gmail.com>
2024-05-27 21:15:04 +0530

    update authentication!!

--- bit_auth/urls.py
+++ bit_auth/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/admin/", views.login_admin_bit, name="login_admin_bit"),
-    path("login/", views.login_teachers, name="login_teachers"),
-    path("register/", views.signup_teachers, name="register_teachers"),
+    path("login/", views.login__, name="login"),
+    path("register/", views.signup, name="signup"),
     path("password-reset/", views.password_reset, name="password_reset"),
     path("password-forget/", views.password_forget, name="password_reset_sent"),
     path("logout/", views.logout_bit, name="logout"),
--- bit_auth/views.py
+++ bit_auth/views.py
@@ -2,20 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def login_admin_bit(request):
-    return render(request, 'bit_auth/login_admin_bit.html')
+def login__(request):
+    return render(request, 'auth/login.html.html')
 
-def login_teachers(request):
-    return render(request, 'bit_auth/login_teachers.html')
-
-def signup_teachers(request):
-    return render(request, 'bit_auth/signup_teachers.html')
+def signup(request):
+    return render(request, 'auth/create_account.html')
 
 def password_reset(request):
-    return render(request, 'bit_auth/password_reset.html')
+    return render(request, 'auth/reset_passwd.html')
 
 def password_forget(request):
-    return render(request, 'bit_auth/password_reset.html')
+    return render(request, 'auth/forget_passwd.html')
 
 def logout_bit(request):
-    return render(request, 'bit_auth/logout_bit.html')
+    return 
