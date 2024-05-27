fd0e6e20457588d0974d24434dc3119788abf075
piyush0431 <rana.workslance@gmail.com>
2024-05-27 21:17:07 +0530

    update auth

--- bit_auth/views.py
+++ bit_auth/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def login__(request):
-    return render(request, 'auth/login.html.html')
+    return render(request, 'auth/login.html')
 
 def signup(request):
     return render(request, 'auth/create_account.html')
