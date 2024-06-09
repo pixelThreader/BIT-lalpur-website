7a35d3d53697f96230bb2515cd8d7154b167c281
piyush0431 <rana.workslance@gmail.com>
2024-06-09 11:51:53 +0530

    update!!!

--- tests/mail.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-# Get the current date
-current_date = datetime.date.today()
-
-# Extract and print the current year
-current_year = current_date.year
-print("Current year:", current_year)
