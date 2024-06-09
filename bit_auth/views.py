66bfa9490347df5e6d302ba9cdb979a4fd282029
piyush0431 <rana.workslance@gmail.com>
2024-06-10 02:11:06 +0530

    update!!!!!

--- bit_auth/views.py
+++ bit_auth/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+import datetime
 
 # Create your views here.
 
 def login__(request):
-    return render(request, 'auth/login.html')
+    cont = {
+        'year': datetime.datetime.now().year
+    }
+    return render(request, 'auth/login.html', context=cont)
 
 def signup(request):
     return render(request, 'auth/sign_up.html')
