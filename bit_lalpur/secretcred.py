38494e87997ef6090d593113ce9f8784d9086e92
piyush0431 <rana.workslance@gmail.com>
2024-06-09 08:21:46 +0530

    update irritation!!!!!😖

---
+++ bit_lalpur/secretcred.py
@@ -0,0 +1,5 @@
+def getcred(what):
+    if what == 'pass':
+        return 'Tesla@2000#piyush*Rana'
+    if what == 'usrnm':
+        return 'electriccars649@gmail.com'
--- bit_lalpur/settings.py
+++ bit_lalpur/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from .secretcred import getcred
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = getcred('usrnm')
+EMAIL_HOST_PASSWORD = getcred('pass')
